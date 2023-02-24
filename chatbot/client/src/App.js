512edc7cc8c1fa234996d69158dd23eef5a629b0
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2023-02-23 21:25:29 -0500

    added starter code

---
+++ chatbot/client/src/App.js
@@ -0,0 +1,42 @@
+// import
+    // function App() {
+    //   return (
+    //     <>
+    //       <div className="app">
+    //         <div className="container">
+    //           <h1>
+    //             Simple chatbot made by{" "}
+    //             <span style={{ color: "red" }}>Coach</span>
+    //           </h1>
+    //         </div>
+    //         <div className="container">
+    //           <h4>1. 
+
+    //           </h4>
+    //         </div>
+    //         <div className="container">
+    //           <h4>2. </h4>
+    //         </div>
+    //         <div className="container">
+    //           <h4>
+    //             3. 
+    //           </h4>
+    //         </div>
+    //         <div className="container">
+    //           <h4>
+    //             4. 
+    //           </h4>
+    //         </div>
+    //         <div className="App mx-auto col-md-6 col-sm-8 col-lg-4 cpl-xl-3 my-5">
+    //           <Chatbot
+    //             config={Config}
+    //             messageParser={MessageParser}
+    //             actionProvider={ActionProvider}
+    //           />
+    //         </div>
+    //       </div>
+    //     </>
+    //   );
+    // }
+
+    // export default App;
